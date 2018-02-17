3f9a87b92bbab2a586b5d5cc09c33de41b86624d
Fabio Hisamoto <fhisamoto@gmail.com>
2018-02-17 13:17:08 -0200

    PositionTest refactoring.

--- src/test/java/mars/app/PositionTest.java
+++ src/test/java/mars/app/PositionTest.java
@@ -8,46 +8,46 @@ public class PositionTest {
 
     @Test
     public void formatsPosition() {
-        assertEquals("(0,0,N)", (new Position(0,0, Direction.N)).toString());
+        assertEquals("(0,0,N)", (new Position(0, 0, Direction.N)).toString());
     }
 
     @Test
-    public void stepsNorth() throws OutOfBoundsException{
-        assertEquals("(0,1,N)", (new Position(0,0, Direction.N).step().toString()));
+    public void stepsNorth() throws Exception {
+        assertEquals(new Position(0, 1, Direction.N), new Position(0, 0, Direction.N).step());
     }
 
     @Test(expected = OutOfBoundsException.class)
-    public void failsToStepNorth() throws OutOfBoundsException {
-        (new Position(0,5, Direction.N)).step();
+    public void failsToStepNorth() throws Exception {
+        (new Position(0, 5, Direction.N)).step();
     }
 
     @Test
-    public void stepsEast() throws OutOfBoundsException{
-        assertEquals("(2,0,E)", (new Position(1,0, Direction.E).step().toString()));
+    public void stepsEast() throws Exception {
+        assertEquals(new Position(2, 0, Direction.E), new Position(1, 0, Direction.E).step());
     }
 
     @Test(expected = OutOfBoundsException.class)
-    public void failsToStepEast() throws OutOfBoundsException {
-        (new Position(5,0, Direction.E)).step();
+    public void failsToStepEast() throws Exception {
+        (new Position(5, 0, Direction.E)).step();
     }
 
     @Test
-    public void stepsSouth() throws OutOfBoundsException{
-        assertEquals("(2,1,S)", (new Position(2,2, Direction.S).step().toString()));
+    public void stepsSouth() throws Exception {
+        assertEquals(new Position(2, 1, Direction.S), new Position(2, 2, Direction.S).step());
     }
 
     @Test(expected = OutOfBoundsException.class)
-    public void failsToStepSouth() throws OutOfBoundsException {
-        (new Position(2,0, Direction.S)).step();
+    public void failsToStepSouth() throws Exception {
+        (new Position(2, 0, Direction.S)).step();
     }
 
     @Test
-    public void stepsWest() throws OutOfBoundsException{
-        assertEquals("(2,1,W)", (new Position(3,1, Direction.W).step().toString()));
+    public void stepsWest() throws Exception {
+        assertEquals(new Position(2, 1, Direction.W), new Position(3, 1, Direction.W).step());
     }
 
     @Test(expected = OutOfBoundsException.class)
-    public void failsToStepWest() throws OutOfBoundsException {
-        (new Position(0,1, Direction.W)).step();
+    public void failsToStepWest() throws Exception {
+        (new Position(0, 1, Direction.W)).step();
     }
 }
